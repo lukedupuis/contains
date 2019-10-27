1a250a74b352b12e8bafdd16be9e72d85570c9f7
Luke Dupuis <lukedupuis@gmail.com>
2019-10-26 18:58:23 -0700

    Adding the main file and providing a description

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # contains
 A function to find a string
+
+## Description
+Given one string or array of strings in which to search, and one string or array of strings for which to search, the function returns a boolean indicating whether the string was found.
+
+### Parameters
+
+    searchIn: string|string[]
+    searchFor: string|string[]
+
+### Returns
+
+    boolean
---
+++ contains.js
@@ -0,0 +1,15 @@
+const contains = (searchIn, searchFor) => {
+  const contains = (searchIn, searchFor) => searchIn.indexOf(searchFor) != -1;
+  if (typeof searchIn == 'string' && Array.isArray(searchFor)) {
+    var found = false;
+    for (var i = 0; i < searchFor.length; i++) {
+      if (found) { break; }
+      found = contains(searchIn, searchFor[i]);
+    }
+    return found;
+  } else {
+    return contains(searchIn, searchFor);
+  }
+};
+
+module.exports = contains;
