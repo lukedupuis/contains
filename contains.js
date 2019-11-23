3cc672723f7d6d838f23682a727c1f64008366fc
Luke Dupuis <lukedupuis@gmail.com>
2019-11-22 19:56:26 -0800

    0.0.1-Handling empty searchIn param

--- contains.js
+++ contains.js
@@ -1,5 +1,5 @@
 const contains = (searchIn, searchFor) => {
-  const contains = (searchIn, searchFor) => searchIn.indexOf(searchFor) != -1;
+  const contains = (searchIn, searchFor) => searchIn && searchIn.indexOf(searchFor) != -1;
   if (typeof searchIn == 'string' && Array.isArray(searchFor)) {
     var found = false;
     for (var i = 0; i < searchFor.length; i++) {
